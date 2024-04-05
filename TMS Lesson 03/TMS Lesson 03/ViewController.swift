c334a68f4d3f41e8379558f070e0acd4d993492f
Yana Astashonok <diableivan@gmail.com>
2024-04-05 19:33:04 +0200

    created new project for lesson 03

---
+++ TMS Lesson 03/TMS Lesson 03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMS Lesson 03
+//
+//  Created by Yana Astashonak on 05/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
